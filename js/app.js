39fff28274df95cb73722bf4a83c193fed8b38d3
tektechnologies <info@tektechnologies.com>
2022-01-22 12:02:00 -0600

    add new call with objects.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,97 @@
 'use strict';
 console.log('app js connected');
+
+/*
+  Practice domain modeling by planning out an app  that allows users to choose their favorite between two pizzas
+  App Flow:
+  - Randomly put 2 pizzas on the screen
+    - Random number generator
+    - a function to display pizzas
+  - A user clicks on a pizza
+    - event listener needs to be on the image to fire the event handler
+    - the event handler fires
+      - ? check if (let totalClicks >= 5) ?
+        - stop letting the user click remove eventlistener
+        - display the clicks
+      -? If not ?
+        - track which pizza was clicked on
+        - update click++ images count of how many times it has been clicked on
+        - update both images'count of times shown++
+        - Randomly Pick 2 pizzas, run the same code that put them on the screen to begin with
+
+  HTML
+    - have a left and right image container in the html
+    - Give them id's so we can select them
+    - let the user know what they are supposed to do
+      - instructions
+
+  More javascript details
+  We need Objects with all the image properties
+  const Image = function ()
+  {
+    name : 'Chicago Pizza',
+    clicks: 0,
+    timesShown: 0,
+    url : 'chicagoPizza.jpeg'
+  }
+
+  We need an Array to hold all image Objects
+
+  function to randomly pick an image{
+    Prevent last picked pizzas from being picked
+      - STRETCH pick all pizzas evenly as possible
+    Math.floor  Math.random() * array.length()
+    make sure left and right image are unique
+  }
+
+  click on an image, targetted by id
+  add event listener('click', function(){
+    keep track of the image that is clicked
+    prevent all currently displayed images from being re clicked {
+    }
+  })
+
+  function to display all the clicks at the end () {
+    generate a table or list
+    populate the data - adds to the inner html of an existing element (table or list)
+    divide object's times clicked by total shown
+  }
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//create pizza objects 
+new PizzaPicture('Brick Oven Pizza', 'assets/images/brickOvenPizza.jpg');
+new PizzaPicture('Calzone', 'assets/images/calzonePizza.jpg');
+new PizzaPicture('Chicago Deep Dish', 'assets/images/chicagoPizza.jpg');
+new PizzaPicture('Chicago Pizza and Oven Grinder', 'assets/images/cpoGinderPizza.jpg');
+new PizzaPicture('Detroit Style', 'assets/images/detroitPizza.jpg');
+new PizzaPicture('Papa Vito\'s Thin', 'assets/images/mwDeluxePizzaThinCrust.jpg');
+new PizzaPicture('New York Thin', 'assets/images/newYorkPizza.jpg');
+new PizzaPicture('Shot Gun Dans', 'assets/images/sgDansHtossedMeatLovPizza.jpg');
