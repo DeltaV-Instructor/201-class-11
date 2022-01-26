03cbcd809452ea9ea6bd597deb396075fe711637
tektechnologies <info@tektechnologies.com>
2022-01-26 11:40:45 -0600

    add grid update and flex

--- js/app.js
+++ js/app.js
@@ -60,7 +60,7 @@ console.log('app js connected');
 
 
 // lab 12 requirements
-/** 
+/**
 As a marketeer, I want to prevent users from seeing the same image in two subsequent iterations, so that they are not biased.
 
 Update your algorithm to randomly generate three unique product images from the images directory.
@@ -79,15 +79,20 @@ The bar charts should only appear after all voting data has been collected.
 
 
 //Global Variables
-let pizzaImageSectionTag = document.getElementById('all_pizzas');
-let rightPizzaImageTag = document.getElementById('left_pizza_img');
-let leftPizzaImageTag = document.getElementById('right_pizza_img');
-
 let totalClicks = 0;
-
+//Add new array-----------------------------------------------
+const allPizzas = [];
+let previouslyPickedPizzas = [];
+let leftPizzaOnThePage;
+let rightPizzaOnThePage;
+// References to the DOM TODO:
+let pizzaImageSectionTag = document.getElementById('all_pizzas');
+let rightPizzaImage = document.getElementById('left_pizza_img');
+let leftPizzaImage = document.getElementById('right_pizza_img');
+// console.log('',{leftPizzaImage, rightPizzaImage});
 //the variable to store the pizzas that are already on the page
-let leftPizzaOnThePage = null;
-let rightPizzaOnThePage = null;
+
+
 
 
 
@@ -99,47 +104,59 @@ let rightPizzaOnThePage = null;
 //Constructor Function Expression
 const PizzaPicture = function(name, imageSrc){
   this.name = name;
-  this.url = imageSrc;
+  this.imageSrc = imageSrc;
   //count our pizza votes
   this.clicks = 0;
   this.timesShown = 0;
   //push into an array
-  PizzaPicture.allImages.push(this);
+  // PizzaPicture.allImages.push(this);
+  allPizzas.push(this);
 
 
 };
-PizzaPicture.allImages = [];
+// PizzaPicture.allImages = [];
+
+console.log(allPizzas);
+
+//5. Comment out the no longer using functions ---------------------------------
+// const renderNewPizzas = function(leftIndex, rightIndex){
+//   // console.log(PizzaPicture.allImages[leftIndex].url);
+//   // console.log(PizzaPicture.allImages[rightIndex].url);
+//   leftPizzaImage.src = PizzaPicture.allImages[leftIndex].url;
+//   rightPizzaImage.src = PizzaPicture.allImages[rightIndex].url;
+// };
 
 
-const renderNewPizzas = function(leftIndex, rightIndex){
-  // console.log(PizzaPicture.allImages[leftIndex].url);
-  // console.log(PizzaPicture.allImages[rightIndex].url);
-  leftPizzaImageTag.src = PizzaPicture.allImages[leftIndex].url;
-  rightPizzaImageTag.src = PizzaPicture.allImages[rightIndex].url;
-};
 
-
-
+//5. Comment out the no longer using functions ---------------------------------
 
 //add function for getting new pizza objects from array based on the index number we get back from out random generator.
-const pickNewPizzas = function(){
-  const leftIndex = Math.floor(Math.random() * PizzaPicture.allImages.length);
-  console.log('left index for the left image', leftIndex);
+// const pickNewPizzas = function(){
+//   const leftIndex = Math.floor(Math.random() * PizzaPicture.allImages.length);
+//   console.log('left index for the left image', leftIndex);
 
-  let rightIndex;
-  do {
-    rightIndex = Math.floor(Math.random() * PizzaPicture.allImages.length);
-  } while(rightIndex === leftIndex);
-  console.log(PizzaPicture.allImages[leftIndex].name + ' and ' + PizzaPicture.allImages[rightIndex].name);
+//   let rightIndex;
+//   do {
+//     rightIndex = Math.floor(Math.random() * PizzaPicture.allImages.length);
+//   } while(rightIndex === leftIndex);
+//   console.log(PizzaPicture.allImages[leftIndex].name + ' and ' + PizzaPicture.allImages[rightIndex].name);
 
 
-  leftPizzaOnThePage = PizzaPicture.allImages[leftIndex];
-  rightPizzaOnThePage = PizzaPicture.allImages[rightIndex];
+//   leftPizzaOnThePage = PizzaPicture.allImages[leftIndex];
+//   rightPizzaOnThePage = PizzaPicture.allImages[rightIndex];
 
-  // console.log('how do these relate to our if below', {leftPizzaOnThePage, rightPizzaOnThePage});
-  renderNewPizzas(leftIndex, rightIndex);
+//   // console.log('how do these relate to our if below', {leftPizzaOnThePage, rightPizzaOnThePage});
+//   renderNewPizzas(leftIndex, rightIndex);
 
-};
+// };
+
+
+
+
+
+
+
+
 
 
 //4.
@@ -151,45 +168,129 @@ const pickNewPizzas = function(){
 
 
 
-const handleClickOnPizza = function(event){
-  console.log('clicking on the picture', event.target);
-
-
-  //how many times do we run the vote? 5 time.s
-  if(totalClicks < 5){
-    const thingWeClickedOn = event.target;
-    // console.log('why',thingWeClickedOn.id);
-    const id = thingWeClickedOn.id;
-
-
-    if(id === 'left_pizza_img' || id === 'right_pizza_img'){
-      //track the pizzas
-      //increment the image in the left slot
-      if(id === 'left_pizza_img'){
-        leftPizzaOnThePage.clicks++;
-      }
-      if(id === 'right_pizza_img'){
-        rightPizzaOnThePage.clicks++;
-      }
-
-      leftPizzaOnThePage.timesShown++;
-      rightPizzaOnThePage.timesShown++;
-
-      pickNewPizzas();
-    }
-
-  }//if totalclick is 5
 
 
 
 
 
+
+
+
+
+
+
+
+
+function handleClickOnPizza(event){
+  // console.log('clicking on the picture', event.target);
+  if(event.target.tagName !== 'IMG'){
+    console.log('not clicking on pic!');
+    return;
+  }
+  console.log(event.target.tagName);
+  console.log(event.target.id);
+  //increment total clicks
   totalClicks++;
-  console.log(totalClicks);
+  console.log('total clicks',totalClicks);
+  //how many times do we run the vote? 5 time.s
+  // if(totalClicks < 5){
+  // const thingWeClickedOn = event.target;
+  // console.log('why',thingWeClickedOn.id);
+  // const id = thingWeClickedOn.id;
+  // if(id === 'left_pizza_img' || id === 'right_pizza_img'){
+  //track the pizzas
+  //increment the image in the left slot
+
+  //refactor this with new id --------------------------
+  if(event.target.id === 'left_pizza_img'){
+    //log to see diff between getting id and then getting src
+    console.log(leftPizzaOnThePage);
+    leftPizzaOnThePage.clicks++;
+    console.log('did we see the click count',leftPizzaOnThePage.clicks);
+  }
+  if(event.target.id === 'right_pizza_img'){
+    rightPizzaOnThePage.clicks++;
+  }
+  // leftPizzaOnThePage.timesShown++;
+  // rightPizzaOnThePage.timesShown++;
+  // pickNewPizzas();
+  // }
+  // }//if totalclick is 5
+
+
+
+  //--------------------------------------------------------------
+
+  // Add the pick pizza image to avoid duplicates and no repeatable showings
+  //Pick two new pizzas
+  const tempPickedPizzas = [];
+  let leftPizzaIndex;
+  do{
+    leftPizzaIndex = Math.floor(Math.random() * allPizzas.length);
+  } while (
+    previouslyPickedPizzas.includes(allPizzas[leftPizzaIndex]) ||
+    tempPickedPizzas.includes(allPizzas[leftPizzaIndex])
+  );
+  //Then update the temp picked pizzas array with new image.
+  tempPickedPizzas.push(allPizzas[leftPizzaIndex]);
+  console.log('temp array', tempPickedPizzas);
+  //--------------------------------------------------------------
+
+  let rightPizzaIndex;
+  do{
+    rightPizzaIndex = Math.floor(Math.random() * allPizzas.length);
+  } while (
+    previouslyPickedPizzas.includes(allPizzas[rightPizzaIndex]) ||
+    tempPickedPizzas.includes(allPizzas[rightPizzaIndex])
+  );
+  //Then update the temp picked pizzas array with new image.
+  tempPickedPizzas.push(allPizzas[rightPizzaIndex]);
+
+
+
+  //--------------------------------------------------------------
+
+  // Now set the new indexs up for the next objects to be seen and tracked
+  leftPizzaOnThePage = allPizzas[leftPizzaIndex];
+  rightPizzaOnThePage = allPizzas[rightPizzaIndex];
+  console.log({leftPizzaOnThePage, rightPizzaOnThePage});
+
+  //now update src with new url from object to display the objects on the page.
+  leftPizzaImage.src = leftPizzaOnThePage.imageSrc;
+  rightPizzaImage.src = rightPizzaOnThePage.imageSrc;
+  console.log({leftPizzaImage, rightPizzaImage});
+
+  //reset previously picked images for randomization
+  previouslyPickedPizzas = [];
+  previouslyPickedPizzas.push(allPizzas[leftPizzaIndex]);
+  previouslyPickedPizzas.push(allPizzas[rightPizzaIndex]);
+
+
+
+
+
+
   if(totalClicks === 5){
     pizzaImageSectionTag.removeEventListener('click', handleClickOnPizza);
+    //Add call to the makeAPizzaChart();
   }
 };//closes the function to handle the click
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -218,11 +319,11 @@ new PizzaPicture('Detroit Style', 'assets/images/detroitPizza.jpg');
 new PizzaPicture('New York Thin', 'assets/images/newYorkPizza.jpg');
 new PizzaPicture('Shot Gun Dans', 'assets/images/sgDansHtossedMeatLovPizza.jpg');
 //1. HERE TOO -----------------------------------------------------
-// When I first load the page, I need to know which goat is left and right, so they can track their clicks in the javascript
-leftPizzaOnThePage = PizzaPicture.allImages[0];
-rightPizzaOnThePage = PizzaPicture.allImages[1];
-
-pickNewPizzas();
+// When I first load the page, I need to know which pizza is left and right, so they can track their clicks in the javascript
+leftPizzaOnThePage = allPizzas[0];
+rightPizzaOnThePage = allPizzas[1];
+console.log('',{leftPizzaOnThePage, rightPizzaOnThePage});
+// pickNewPizzas();
 
 
 //2. THEN LATER WE WILL ADD THE CHART IN LAST
