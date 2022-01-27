9fa1a39bb22dab63207c28eb02250f2b171609a3
tektechnologies <info@tektechnologies.com>
2022-01-26 18:43:57 -0600

    clean up js add steps

--- js/app.js
+++ js/app.js
@@ -1,65 +1,6 @@
 'use strict';
 console.log('app js connected');
 
-/*
-  Practice domain modeling by planning out an app  that allows users to choose their favorite between two pizzas
-  App Flow:
-  - Randomly put 2 pizzas on the screen
-    - Random number generator
-    - a function to display pizzas
-  - A user clicks on a pizza
-    - event listener needs to be on the image to fire the event handler
-    - the event handler fires
-      - ? check if (let totalClicks >= 5) ?
-        - stop letting the user click remove eventlistener
-        - display the clicks
-      -? If not ?
-        - track which pizza was clicked on
-        - update click++ images count of how many times it has been clicked on
-        - update both images'count of times shown++
-        - Randomly Pick 2 pizzas, run the same code that put them on the screen to begin with
-
-  HTML
-    - have a left and right image container in the html
-    - Give them id's so we can select them
-    - let the user know what they are supposed to do
-      - instructions
-
-  More javascript details
-  We need Objects with all the image properties
-  const Image = function ()
-  {
-    name : 'Chicago Pizza',
-    clicks: 0,
-    timesShown: 0,
-    url : 'chicagoPizza.jpeg'
-  }
-
-  We need an Array to hold all image Objects
-
-  function to randomly pick an image{
-    Prevent last picked pizzas from being picked
-      - STRETCH pick all pizzas evenly as possible
-    Math.floor  Math.random() * array.length()
-    make sure left and right image are unique
-  }
-
-  click on an image, targetted by id
-  add event listener('click', function(){
-    keep track of the image that is clicked
-    prevent all currently displayed images from being re clicked {
-    }
-  })
-
-  function to display all the clicks at the end () {
-    generate a table or list
-    populate the data - adds to the inner html of an existing element (table or list)
-    divide object's times clicked by total shown
-  }
-
-*/
-
-
 
 
 
@@ -78,6 +19,12 @@ let rightPizzaOnThePage = null;
 
 
 
+
+//3.HERE WE WILL UPDATE OUR CONSTRUCTOR TO USE A GLOBAL ARRAY OF OBJECTS
+// =======================================
+// Constructors
+// =======================================
+
 //Constructor Function Expression
 const PizzaPicture = function(name, imageSrc){
   this.name = name;
@@ -93,63 +40,31 @@ const PizzaPicture = function(name, imageSrc){
 PizzaPicture.allImages = [];
 
 
-const renderNewPizzas = function(leftIndex, rightIndex){
-  // console.log(PizzaPicture.allImages[leftIndex].url);
-  // console.log(PizzaPicture.allImages[rightIndex].url);
-  leftPizzaImageTag.src = PizzaPicture.allImages[leftIndex].url;
-  rightPizzaImageTag.src = PizzaPicture.allImages[rightIndex].url;
-};
 
 
 
 
-//add function for getting new pizza objects from array based on the index number we get back from out random generator.
-const pickNewPizzas = function(){
-  const leftIndex = Math.floor(Math.random() * PizzaPicture.allImages.length);
-  console.log('left index for the left image', leftIndex);
-  let rightIndex;
-  do {
-    rightIndex = Math.floor(Math.random() * PizzaPicture.allImages.length);
-  } while(rightIndex === leftIndex);
-  // console.log(PizzaPicture.allImages[leftIndex].name + ' and ' + PizzaPicture.allImages[rightIndex].name);
-  leftPizzaOnThePage = PizzaPicture.allImages[leftIndex];
-  rightPizzaOnThePage = PizzaPicture.allImages[rightIndex];
-  // console.log('how do these relate to our if below', {leftPizzaOnThePage, rightPizzaOnThePage});
-  renderNewPizzas(leftIndex, rightIndex);
-};
-
-
-
-
+//4.
+// =======================================
+// Other Functions
+// =======================================
 
 const handleClickOnPizza = function(event){
   console.log('clicking on the picture', event.target);
 
 
-  //how many times do we run the vote? 5 time.s
-  if(totalClicks < 5){
-    const thingWeClickedOn = event.target;
-    // console.log('why',thingWeClickedOn.id);
-    const id = thingWeClickedOn.id;
 
 
-    if(id === 'left_pizza_img' || id === 'right_pizza_img'){
-      //track the pizzas
-      //increment the image in the left slot
-      if(id === 'left_pizza_img'){
-        leftPizzaOnThePage.clicks++;
-      }
-      if(id === 'right_pizza_img'){
-        rightPizzaOnThePage.clicks++;
-      }
+  if(id === 'left_pizza_img'){
+    leftPizzaOnThePage.clicks++;
+  }
+  if(id === 'right_pizza_img'){
+    rightPizzaOnThePage.clicks++;
+  }
 
-      leftPizzaOnThePage.timesShown++;
-      rightPizzaOnThePage.timesShown++;
+  leftPizzaOnThePage.timesShown++;
+  rightPizzaOnThePage.timesShown++;
 
-      pickNewPizzas();
-    }
-
-  }//if totalclick is 5
 
 
 
@@ -169,10 +84,13 @@ const handleClickOnPizza = function(event){
 
 
 
+// 1. START HERE
+// ===================================
+// Initialize the Page
+// ===================================
 
 //add in event listener
 pizzaImageSectionTag.addEventListener('click', handleClickOnPizza);
-
 
 
 
@@ -191,4 +109,11 @@ new PizzaPicture('Shot Gun Dans', 'assets/images/sgDansHtossedMeatLovPizza.jpg')
 leftPizzaOnThePage = PizzaPicture.allImages[0];
 rightPizzaOnThePage = PizzaPicture.allImages[1];
 
-pickNewPizzas();
+
+
+
+
+//2. THEN LATER WE WILL ADD THE CHART IN LAST
+// ==================================
+// ChartJs Implementation
+// ==================================
